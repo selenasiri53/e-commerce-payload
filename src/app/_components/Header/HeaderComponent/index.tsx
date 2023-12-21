f7c863a13c54d9921cd55fea75677f177560ff3f
selenasiri53 <selenasiri53@gmail.com>
2023-12-21 15:51:48 -0500

    noHeaderFooterUrls

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -1,26 +1,32 @@
+'use client'
+
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 import { Header } from '../../../../payload/payload-types'
+import { noHeaderFooterUrls } from '../../../constants'
 import { Gutter } from '../../Gutter'
-import Link from 'next/link'
-
-import Image from 'next/image'
 import { HeaderNav } from '../Nav'
-import MobileNav from '../MobileNav'
+// import { MobileNav } from '../MobileNav'
 
 import classes from './index.module.scss'
 
 const HeaderComponent = ({ header }: { header: Header }) => {
+    const pathname = usePathname()
+
   return (
-    <nav className={[classes.Header]}>
-        {/* will handle error later^: Type 'string[]' is not assignable to type 'string' */}
+    <nav 
+        className={[classes.header, noHeaderFooterUrls.includes(pathname) && classes.hide].filter(Boolean).join(' ')}>
+        {/* in navigation, filter the ones that are true and join with an empty space */}
         <Gutter className={classes.wrap}>
             <Link href="/">
                 <Image src="/logo-black.svg" alt="logo" width={170} height={50} />
             </Link>
 
             <HeaderNav header={header} />
-            <MobileNav header={header} />
+            {/* <MobileNav header={header} /> */}
         </Gutter>
     </nav>
   )
